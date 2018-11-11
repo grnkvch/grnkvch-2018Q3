556d3cd9e47376f18688dc29689b6aa1af7eb1fd
grnkvch <grinkevich.vv@gmail.com>
2018-11-11 15:45:48 +0300

    feat: make dots clickable

--- codejam-dom/notifications/notifications.js
+++ codejam-dom/notifications/notifications.js
@@ -1,8 +1,8 @@
-var tipMassive = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+let tipMassive = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                 "Suspendisse turpis enim, dapibus in libero vitae, tempor ornare massa.",
                 "Morbi feugiat congue nisl, elementum hendrerit elit elementum vel.", 
                 "Nullam vel faucibus velit."];
-var tipCount = tipMassive.length;
+let tipCount = tipMassive.length;
 
 function activate (elem) {
     elem.classList.toggle("active");
@@ -36,37 +36,56 @@ function next () {
     let currElems = document.querySelectorAll(".notifer .active");
     let currElemIndex = parseInt(currElems[0].getAttribute("number"));
     if (currElemIndex<tipCount-1) {
-        currElems.forEach((item)=>activate(item));
-        let nextElems = document.querySelectorAll('[number="'+(currElemIndex+1)+'"]');
-        nextElems.forEach((item)=>activate(item));
+        var nextElems = document.querySelectorAll('[number="'+(currElemIndex+1)+'"]');
     } else {
-        currElems.forEach((item)=>activate(item));
-        let nextElems = document.querySelectorAll('[number="'+(0)+'"]');
-        nextElems.forEach((item)=>activate(item));
+        var nextElems = document.querySelectorAll('[number="'+(0)+'"]');
     } 
+    switchTips(currElems, nextElems);
 }
 
 function previous () {
     let currElems = document.querySelectorAll(".notifer .active");
     let currElemIndex = parseInt(currElems[0].getAttribute("number"));
     if (currElemIndex>0) {
-        currElems.forEach((item)=>activate(item));
-        let nextElems = document.querySelectorAll('[number="'+(currElemIndex-1)+'"]');
-        nextElems.forEach((item)=>activate(item));
+        var nextElems = document.querySelectorAll('[number="'+(currElemIndex-1)+'"]');
     } else {
-        currElems.forEach((item)=>activate(item));
-        let nextElems = document.querySelectorAll('[number="'+(tipCount-1)+'"]');
-        nextElems.forEach((item)=>activate(item));
-    } 
+        var nextElems = document.querySelectorAll('[number="'+(tipCount-1)+'"]');
+    }
+    switchTips(currElems, nextElems);
 }
 
+function switchTips (previous,next) {
+    previous.forEach((item)=>activate(item));
+    next.forEach((item)=>activate(item));
+}
 
+function dotClick (event) {
+    let target = event.target;
+    while (target != navdots) {
+        if (target.tagName == 'LI') {
+            let currElems = document.querySelectorAll(".notifer .active");
+            let nextElemIndex = parseInt(target.firstElementChild.getAttribute("number"));
+            let nextElems = document.querySelectorAll('[number="'+(nextElemIndex)+'"]');
+            switchTips(currElems, nextElems);
+            return
+        }
+        target = target.parentNode;
+    }
+}
+
+function close () {
+    activate(notificator);
+}
+
+let notificator = document.querySelector(".notifer");
 let closebtn = document.querySelector(".close");
 let leftarrow = document.querySelector(".arrow.left");
 let rightarrow = document.querySelector(".arrow.right");
+let navdots = document.querySelector(".navdots");
 
 leftarrow.addEventListener("click", previous);
 rightarrow.addEventListener("click", next);
-closebtn.addEventListener("click",()=>activate(document.querySelector('.notifer')));
+closebtn.addEventListener("click",close);
+navdots.addEventListener("click", dotClick);
 document.addEventListener("DOMContentLoaded", ()=>setContent (tipMassive));
 document.addEventListener("DOMContentLoaded", setTimeout(()=>activate(document.querySelector('.notifer')),5000));
