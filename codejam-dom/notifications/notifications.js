ff9e74188c6c9272f14d7edefe38d50bf532dc6a
grnkvch <grinkevich.vv@gmail.com>
2018-11-11 17:50:18 +0300

    refactor: code changes according airbnb style guide

--- codejam-dom/notifications/notifications.js
+++ codejam-dom/notifications/notifications.js
@@ -1,127 +1,131 @@
-let tipMassive = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                "Donec eget velit eu sem consectetur molestie ut ut eros. Cras ut sapien sed nisi placerat vestibulum sit amet vel justo. Proin facilisis lorem vitae maximus vulputate. Aliquam suscipit mi nec eros dictum tempus. Praesent viverra mi nec viverra ultricies.",
-                "Morbi feugiat congue nisl, elementum hendrerit elit elementum vel.", 
-                "Nullam vel faucibus velit.",
-                "Cras ut sapien sed nisi placerat vestibulum sit amet vel justo.",
-                "Proin facilisis lorem vitae maximus vulputate.",
-                "Aliquam suscipit mi nec eros dictum tempus.",
-                "Praesent viverra mi nec viverra ultricies.",
-                "Nunc vel mi a leo tristique molestie.",
-                "Pellentesque commodo felis sagittis, pellentesque ante vitae, scelerisque lectus."];
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 
-let tipCount = tipMassive.length > 9 ? 9 : tipMassive.length;
+const tips = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Donec eget velit eu sem consectetur molestie ut ut eros. Cras ut sapien sed nisi placerat vestibulum sit amet vel justo. Proin facilisis lorem vitae maximus vulputate. Aliquam suscipit mi nec eros dictum tempus. Praesent viverra mi nec viverra ultricies.',
+  'Morbi feugiat congue nisl, elementum hendrerit elit elementum vel.',
+  'Nullam vel faucibus velit.',
+  'Cras ut sapien sed nisi placerat vestibulum sit amet vel justo.',
+  'Proin facilisis lorem vitae maximus vulputate.',
+  'Aliquam suscipit mi nec eros dictum tempus.',
+  'Praesent viverra mi nec viverra ultricies.',
+  'Nunc vel mi a leo tristique molestie.',
+  'Pellentesque commodo felis sagittis, pellentesque ante vitae, scelerisque lectus.'];
 
-function activate (elem) {
-    elem.classList.toggle("active");
+const tipCount = tips.length > 9 ? 9 : tips.length;
+
+const notificator = document.querySelector('.notifer');
+const closebtn = document.querySelector('.notifer .close');
+const leftarrow = document.querySelector('.notifer .arrow.left');
+const rightarrow = document.querySelector('.notifer .arrow.right');
+const navdots = document.querySelector('.notifer .navdots');
+const checkBox = document.querySelector('.notifer .checkbox input');
+
+function activate(elem) {
+  elem.classList.toggle('active');
 }
 
-function setContent (tipMassive) {
-    let dotList = document.querySelector(".notifer .navdots");
-    let tipList = document.querySelector(".notifer .tipcontainer");
+function setContent(tiparr) {
+  const dotList = document.querySelector('.notifer .navdots');
+  const tipList = document.querySelector('.notifer .tipcontainer');
 
-    for (let i=0; i<tipCount; i++) {
-        let dotElemArea = document.createElement("li");
-        let dotElem = document.createElement("i");
-        let tip = document.createElement("div");
-        dotElemArea.classList.add("dotarea");
-        dotElem.classList.add("dot");
-        dotElem.setAttribute("number", i);
-        tip.classList.add("tip");
-        tip.textContent = tipMassive[i];
-        tip.setAttribute("number", i);
-        if (i==0) {
-            dotElem.classList.add("active");
-            tip.classList.add("active");
-        }
-        dotElemArea.appendChild(dotElem);
-        dotList.appendChild(dotElemArea);
-        tipList.appendChild(tip);
+  for (let i = 0; i < tipCount; i++) {
+    const dotElemArea = document.createElement('li');
+    const dotElem = document.createElement('i');
+    const tip = document.createElement('div');
+    dotElemArea.classList.add('dotarea');
+    dotElem.classList.add('dot');
+    dotElem.setAttribute('number', i);
+    tip.classList.add('tip');
+    tip.textContent = tiparr[i];
+    tip.setAttribute('number', i);
+    if (i === 0) {
+      dotElem.classList.add('active');
+      tip.classList.add('active');
     }
+    dotElemArea.appendChild(dotElem);
+    dotList.appendChild(dotElemArea);
+    tipList.appendChild(tip);
+  }
 }
 
-function next () {
-    let currElems = document.querySelectorAll(".notifer .active");
-    let currElemIndex = parseInt(currElems[0].getAttribute("number"));
-    if (currElemIndex<tipCount-1) {
-        var nextElems = document.querySelectorAll('.notifer [number="'+(currElemIndex+1)+'"]');
-    } else {
-        var nextElems = document.querySelectorAll('.notifer [number="'+(0)+'"]');
-    } 
-    switchTips(currElems, nextElems);
+function switchTips(previousitem, nextitem) {
+  previousitem.forEach(item => activate(item));
+  nextitem.forEach(item => activate(item));
 }
 
-function previous () {
-    let currElems = document.querySelectorAll(".notifer .active");
-    let currElemIndex = parseInt(currElems[0].getAttribute("number"));
-    if (currElemIndex>0) {
-        var nextElems = document.querySelectorAll('.notifer [number="'+(currElemIndex-1)+'"]');
-    } else {
-        var nextElems = document.querySelectorAll('.notifer [number="'+(tipCount-1)+'"]');
+function next() {
+  const currElems = document.querySelectorAll('.notifer .active');
+  const currElemIndex = parseInt(currElems[0].getAttribute('number'));
+  let nextElems;
+  if (currElemIndex < tipCount - 1) {
+    nextElems = document.querySelectorAll(`.notifer [number="${currElemIndex + 1}"]`);
+  } else {
+    nextElems = document.querySelectorAll(`.notifer [number="${0}"]`);
+  }
+  switchTips(currElems, nextElems);
+}
+
+function previous() {
+  const currElems = document.querySelectorAll('.notifer .active');
+  const currElemIndex = parseInt(currElems[0].getAttribute('number'));
+  let nextElems;
+  if (currElemIndex > 0) {
+    nextElems = document.querySelectorAll(`.notifer [number="${currElemIndex - 1}"]`);
+  } else {
+    nextElems = document.querySelectorAll(`.notifer [number="${tipCount - 1}"]`);
+  }
+  switchTips(currElems, nextElems);
+}
+
+function dotClick(event) {
+  let target = event.target;
+  while (target !== navdots) {
+    if (target.tagName === 'LI') {
+      const currElems = document.querySelectorAll('.notifer .active');
+      const nextElemIndex = parseInt(target.firstElementChild.getAttribute('number'));
+      const nextElems = document.querySelectorAll(`.notifer [number="${nextElemIndex}"]`);
+      switchTips(currElems, nextElems);
+      return;
     }
-    switchTips(currElems, nextElems);
+    target = target.parentNode;
+  }
 }
 
-function switchTips (previous,next) {
-    previous.forEach((item)=>activate(item));
-    next.forEach((item)=>activate(item));
+function close() {
+  if (notificator.classList.contains('active')) activate(notificator);
 }
 
-function dotClick (event) {
-    let target = event.target;
-    while (target != navdots) {
-        if (target.tagName == 'LI') {
-            let currElems = document.querySelectorAll(".notifer .active");
-            let nextElemIndex = parseInt(target.firstElementChild.getAttribute("number"));
-            let nextElems = document.querySelectorAll('.notifer [number="'+(nextElemIndex)+'"]');
-            switchTips(currElems, nextElems);
-            return
-        }
-        target = target.parentNode;
-    }
+function toogleNotifer() {
+  if (checkBox.checked) {
+    localStorage.setItem('disableNotification', 'true');
+  } else localStorage.removeItem('disableNotification');
 }
 
-function close () {
-    if (notificator.classList.contains("active")) activate(notificator);
+function showNotification() {
+  if (!localStorage.getItem('disableNotification')) activate(notificator);
 }
 
-function toogleNotifer () {
-    if (checkBox.checked) {
-    localStorage.setItem("disableNotification", "true")
-    }
-    else localStorage.removeItem("disableNotification");
-} 
-
-function showNotification () {
-    if (!localStorage.getItem("disableNotification")) activate(notificator);
+function keybordControl(event) {
+  const key = event.keyCode;
+  switch (key) {
+    case 37:
+      previous();
+      break;
+    case 39:
+      next();
+      break;
+    case 27:
+      close();
+      break;
+  }
 }
 
-function keybordControl (event) {
-    let key = event.keyCode;
-    switch (key) {
-        case 37:
-          previous ();
-          break;
-        case 39:
-          next ();
-          break;
-        case 27:
-          close ();
-          break;
-      }
-}
-
-let notificator = document.querySelector(".notifer");
-let closebtn = document.querySelector(".notifer .close");
-let leftarrow = document.querySelector(".notifer .arrow.left");
-let rightarrow = document.querySelector(".notifer .arrow.right");
-let navdots = document.querySelector(".notifer .navdots");
-let checkBox = document.querySelector(".notifer .checkbox input");
-
-leftarrow.addEventListener("click", previous);
-rightarrow.addEventListener("click", next);
-closebtn.addEventListener("click",close);
-navdots.addEventListener("click", dotClick);
-checkBox.addEventListener("change", toogleNotifer);
-document.addEventListener("DOMContentLoaded", ()=>setContent (tipMassive));
-document.addEventListener("keyup", keybordControl);
-document.addEventListener("DOMContentLoaded", setTimeout(()=>showNotification(), 5000));
+leftarrow.addEventListener('click', previous);
+rightarrow.addEventListener('click', next);
+closebtn.addEventListener('click', close);
+navdots.addEventListener('click', dotClick);
+checkBox.addEventListener('change', toogleNotifer);
+document.addEventListener('DOMContentLoaded', () => setContent(tips));
+document.addEventListener('keyup', keybordControl);
+document.addEventListener('DOMContentLoaded', setTimeout(() => showNotification(), 5000));
