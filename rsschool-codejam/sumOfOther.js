bcd61820c007b89330548a36ab75e4d93886fedc
grnkvch <grinkevich.vv@gmail.com>
2018-11-19 18:28:24 +0300

    feat: HTML layout, CSS styles

--- rsschool-codejam/sumOfOther.js
+++
@@ -1,7 +0,0 @@
-/* eslint linebreak-style: ["error", "windows"] */
-module.exports = function sumOfOther(arr) {
-  return arr
-    .map((item, index, arr) => arr
-      .filter((item, i) => index !== i)
-      .reduce((sum, current) => sum + current));
-};
