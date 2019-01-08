77a97a7e8d75e284da55c266a70b6cdf9bd80e79
grnkvch <grinkevich.vv@gmail.com>
2019-01-08 21:29:43 +0300

    feat: add preloader

---
+++ the_game/src/screens/preloader/preloader.template.js
@@ -0,0 +1,8 @@
+export default `<div id='loader' class="loader done">
+<div class="l_main">
+  <div class="l_square"><span></span><span></span><span></span></div>
+  <div class="l_square"><span></span><span></span><span></span></div>
+  <div class="l_square"><span></span><span></span><span></span></div>
+  <div class="l_square"><span></span><span></span><span></span></div>
+</div>
+</div>`;
