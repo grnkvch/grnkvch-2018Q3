f54f0733d779ef73cd517e5dab2fd6f771cfaf87
grnkvch <grinkevich.vv@gmail.com>
2019-01-08 21:30:11 +0300

    feat: add LandingPage

---
+++ the_game/src/screens/home/home.js
@@ -0,0 +1,3 @@
+import template from './home.template';
+import './home.css';
+document.body.innerHTML = template;
---
+++ the_game/src/screens/home/home.template.js
@@ -0,0 +1,39 @@
+export default `
+<section class="home-screen">
+  <h1 class='home-screen__header-1'>The <span class='color-text-fragment'>Retro</span>Super<span class='color-text-fragment'>Future</span> Game</h1>
+  <div class="home-screen__section home-screen__about-section">
+    <h2 class='home-screen__header-2'>About</h2>
+    <p class='section-text'>
+    Hello my friend!
+    </p>
+    <p class='section-text'>
+    Today you have a hard battle ahead ...
+    </p>
+    <p class='section-text'>
+    The enemy doesn't sleep and  to beat him you will have to use your head very well.
+    </p>
+    <p class='section-text'>
+    The most difficult are mathematical skills and knowledge of foreign languages.
+    </p>
+    <p class='section-text'>
+    But anyway, be careful Adventurer...
+    </p>
+    <p class='section-text'>
+    May the Force be with you!
+    </p>
+  </div>
+  <div class="home-screen__section home-screen__screenShot-section">
+    <h2 class='home-screen__header-2'>ScreenShots</h2>
+  </div>
+  <div class="home-screen__section home-screen__controls-section">
+    <h2 class='home-screen__header-2'>Controls</h2>
+  </div>
+  <div class="home-screen__section home-screen__play-section">
+    <h2 class='home-screen__header-2'>Time for a battle as great as one in Mozambique in 1979</h2>
+    <button class='play-button home-screen__start-button'>PLAY</button>
+  </div>
+  <div class="home-screen__section home-screen__credits-section">
+    <h2 class='home-screen__header-2'>Credits</h2>
+  </div>
+</section>
+`;
