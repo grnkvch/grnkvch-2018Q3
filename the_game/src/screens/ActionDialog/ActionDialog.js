1d90b2e8993ab502f3f159a68e27e0fa17082e89
grnkvch <grinkevich.vv@gmail.com>
2019-01-09 07:54:25 +0300

    feat: add keybordcontrol to  ActionDilog

--- the_game/src/screens/ActionDialog/ActionDialog.js
+++ the_game/src/screens/ActionDialog/ActionDialog.js
@@ -1,6 +1,6 @@
+import KeyCode from 'keycode-js';
 import ActionDialogTemplate from './ActionDialog.template.js';
 import './ActionDialog.css';
-
 
 export default class ActionDialog {
   constructor() {
@@ -10,7 +10,7 @@ export default class ActionDialog {
   static initialize(actionObj) {
     const actionDialog = new ActionDialog();
     actionDialog.content = document.createElement('div');
-    actionDialog.content.classList.add('.action-dialog-screen__wrapper')
+    actionDialog.content.classList.add('.action-dialog-screen__wrapper');
     actionDialog.content.innerHTML = ActionDialogTemplate;
     const keys = Object.keys(actionObj);
     keys.forEach((key) => {
@@ -20,6 +20,8 @@ export default class ActionDialog {
       </button>
       `;
     });
+    actionDialog.content.querySelector('.action-dialog-screen__section')
+      .firstElementChild.classList.toggle('active');
     return actionDialog;
   }
 
@@ -32,16 +34,48 @@ export default class ActionDialog {
     document.querySelector('.modal-window__container').innerHTML = '';
   }
 
+  static toggleActive(prev, next) {
+    prev.classList.toggle('active');
+    next.classList.toggle('active');
+  }
+
   pickAction() {
     this.show();
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+      function keyBoardListener(e) {
+        const activeBtn = document.querySelector('.action-dialog-screen__button.active');
+        if (e.keyCode === KeyCode.KEY_ESCAPE) {
+          ActionDialog.hide();
+          reject();
+          document.removeEventListener('keyup', listener);
+        }
+        if (e.keyCode === KeyCode.KEY_DOWN) {
+          if (activeBtn.nextElementSibling) {
+            ActionDialog.toggleActive(activeBtn, activeBtn.nextElementSibling);
+          } else ActionDialog.toggleActive(activeBtn, activeBtn.parentElement.firstElementChild);
+        }
+        if (e.keyCode === KeyCode.KEY_UP) {
+          if (activeBtn.previousElementSibling) {
+            ActionDialog.toggleActive(activeBtn, activeBtn.previousElementSibling);
+          } else ActionDialog.toggleActive(activeBtn, activeBtn.parentElement.lastElementChild);
+        }
+        if (e.keyCode === KeyCode.KEY_SPACE
+          || e.keyCode === KeyCode.KEY_RETURN || e.keyCode === KeyCode.ENTER) {
+          const actionkey = activeBtn.getAttribute('actionkey');
+          this.resolve(actionkey);
+          ActionDialog.hide();
+          document.removeEventListener('keyup', listener);
+        }
+      }
       this.content.addEventListener('click', (e) => {
         const actionkey = e.target.getAttribute('actionkey');
         if (actionkey) {
           resolve(actionkey);
           ActionDialog.hide();
-        } 
+        }
+        document.removeEventListener('keyup', ActionDialog.keyBoardListener);
       });
+      document.addEventListener('keyup', ActionDialog.keyBoardListener);
     });
   }
 }
