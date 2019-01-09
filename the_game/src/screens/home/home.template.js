0556c8a5729ada8ea7322d6ddfae7edf54e5b112
grnkvch <grinkevich.vv@gmail.com>
2019-01-09 22:48:37 +0300

    feat: add screenshots and description to LandingPage

--- the_game/src/screens/home/home.template.js
+++ the_game/src/screens/home/home.template.js
@@ -22,11 +22,31 @@ export default `
     May the Force be with you!
     </p>
   </div>
-  <div class="home-screen__section home-screen__screenShot-section">
+  <div class="home-screen__section home-screen__screenshot-section">
     <h2 class='home-screen__header-2'>ScreenShots</h2>
+    <div class="home-screen__screenshot-section__wrapper">
+      <img src="./src/images/screenshot_0000.png" alt="screenshot" class="home-screen__screenShot-section__screenshot">
+      <img src="./src/images/screenshot_0001.png" alt="screenshot" class="home-screen__screenShot-section__screenshot">
+      <img src="./src/images/screenshot_0002.png" alt="screenshot" class="home-screen__screenShot-section__screenshot">
+      <img src="./src/images/screenshot_0003.png" alt="screenshot" class="home-screen__screenShot-section__screenshot">
+      <img src="./src/images/screenshot_0004.png" alt="screenshot" class="home-screen__screenShot-section__screenshot">
+      <img src="./src/images/screenshot_0005.png" alt="screenshot" class="home-screen__screenShot-section__screenshot">
+    </div>
   </div>
   <div class="home-screen__section home-screen__controls-section">
     <h2 class='home-screen__header-2'>Controls</h2>
+    <p class='section-text'>
+    You can use keyboard or mouse to fight.
+    </p>
+    <p class='section-text'>
+    Use arrows to navigate.
+    </p>
+    <p class='section-text'>
+    SPACE or ENTER to select.
+    </p>
+    <p class='section-text'>
+    ESCAPE to return.
+    </p>
   </div>
   <div class="home-screen__section home-screen__play-section">
     <h2 class='home-screen__header-2'>Time for a battle as great as one in Mozambique in 1979</h2>
@@ -34,6 +54,14 @@ export default `
   </div>
   <div class="home-screen__section home-screen__credits-section">
     <h2 class='home-screen__header-2'>Credits</h2>
+    <div class="home-screen__credits-section__wrapper">
+    <img src="./src/images/author.png" alt="screenshot" class="home-screen__credits-section__author">
+    <div>
+    <p class='section-text'>This is The Greates and Best Author of this Game! Ave!</p>
+    <p class='section-text'><a href="https://github.com/grnkvch">https://github.com/grnkvch<a></p>
+    </div>
+    </div>
+    <p class='section-text'>Thanks <a href="https://www.kenney.nl/">https://www.kenney.nl/<a> for great gameart</p>
   </div>
 </section>
 `;
