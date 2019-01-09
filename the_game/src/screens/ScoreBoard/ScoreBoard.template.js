37f8fad39f98939587c9c731c295721fea3ab7e6
grnkvch <grinkevich.vv@gmail.com>
2019-01-09 09:59:36 +0300

    feat: add scoreboard

---
+++ the_game/src/screens/ScoreBoard/ScoreBoard.template.js
@@ -0,0 +1,13 @@
+export default `
+<div class="scoreboard-screen">
+    <div class="scoreboard-screen__section">
+    <h1 class="scoreboard-screen__header-1">scoreboard</h1>
+    <table class="scoreboard-screen__table">
+        <tr>
+            <th class="scoreboard-screen__table-header scoreboard-screen__table-header__name">Name</th>
+            <th class="scoreboard-screen__table-header scoreboard-screen__table-header__name">Score</th>
+        </tr>
+    </table>
+    </div>
+    <button class="scoreboard-screen__button"></button>
+</div>`;
