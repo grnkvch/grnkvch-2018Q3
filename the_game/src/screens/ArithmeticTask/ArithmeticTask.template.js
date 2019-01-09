eb87985241cffa8afcfe86ed8c4c5fc3c58e4e26
grnkvch <grinkevich.vv@gmail.com>
2019-01-09 09:59:06 +0300

    feat: add arithmetic and Translation tasks

---
+++ the_game/src/screens/ArithmeticTask/ArithmeticTask.template.js
@@ -0,0 +1,9 @@
+export default `
+<div class="arithmetic-task-screen">
+    <div class="arithmetic-task-screen__section">
+      <p class="arithmetic-task-screen__section-text"></p>
+      <form><div class="arithmetic-task-screen__input-container"><input type='text' class="arithmetic-task-screen__input" autofocus></div>
+      <button type='sumbit' class="arithmetic-task-screen__play-button login-screen__submit-button">GO</button>
+      </form>
+    </div>
+</div>`;
---
+++ the_game/src/screens/TranslationTask/TranslationTaskConfig.js
@@ -0,0 +1,8 @@
+const dictinory = {
+  cat: ['Russian', 'кот', 'кошка', 'котик'],
+  dog: ['Russian', 'собака', 'пес', 'пёс'],
+  собака: ['English', 'dog'],
+  кот: ['English', 'cat'],
+};
+
+export default dictinory;
